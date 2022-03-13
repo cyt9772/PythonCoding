06ad84209585cc26ac459c5a3e33d1f208cf96b0
yutae <cyt9772@naver.com>
2022-03-13 21:44:03 +0900

    sequential/binary search

--- ex187_sequencial.py
+++ ex187_sequencial.py
@@ -6,7 +6,18 @@ def sequencial_search(data, target):
         else:
             continue
 
-
+def binary_search(data, target, start, end):
+    if start>end:
+        return None
+    mid=int((start+end)/2)
+    if data[mid]==target:
+        return mid
+    elif data[mid]<target:
+        start=mid+1
+    elif data[mid]>target:
+        end=mid-1
+    return binary_search(data, target, start, end)
+""" sequential search    
 input_data=input().split()
 n=int(input_data[0])
 target=input_data[1]
@@ -14,4 +25,12 @@ target=input_data[1]
 data=list(input().split())
 
 print(sequencial_search(data,target))
+"""
+""" 
+binary search
+"""
+n,target=map(int, input().split())
+data=list(map(int, input().split()))
+
+print(binary_search(data, target, 0, len(data)))
 
